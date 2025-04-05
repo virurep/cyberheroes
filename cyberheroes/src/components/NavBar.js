f17fa9f43a8c1f6ed58cebc36e007c24a4d78e41
mattchung25 <mchung25@uw.edu>
2025-04-04 17:11:03 -0700

    css and navbar changes

--- cyberheroes/src/components/NavBar.js
+++ cyberheroes/src/components/NavBar.js
@@ -6,8 +6,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <img src={logo} alt="Cyberheroes Logo" className="navbar-logo" />
-        <div className="navbar-title">Cyberheroes</div>
+        <Link to="/" className="logo-link">
+          <img src={logo} alt="Cyberheroes Logo" className="navbar-logo" />
+        </Link>
+        <Link to="/" className="title-link">
+          <div className="navbar-title">Cyberheroes</div>
+        </Link>
       </div>
       <div className="navbar-right">
         <Link to="/exploration-map" className="home-button">Home</Link>
