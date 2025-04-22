21f4d65df9866287f56d226654b4e1f38b8e0b94
mattchung25 <mchung25@uw.edu>
2025-04-22 13:21:16 -0700

    made the transition page sort of dynamic

--- cyberheroes/src/components/Transition.js
+++ cyberheroes/src/components/Transition.js
@@ -1,9 +1,25 @@
 import React from "react";
 import "../styles/transitions.css";
 import Navbar from './NavBar';
-import allieImage from '../img/characters/allie.png'; // Make sure this path is correct
+import Transitions from '../data/transitions.json';
 
 const Transition = () => {
+    const characterImages = require.context('../img/characters', false, /\.(png|jpe?g|svg)$/);
+
+    const transitionData = Transitions.privacy_planet;
+    console.log(transitionData);
+
+    const currQuiz = transitionData['quiz-3'];
+    console.log(currQuiz);
+
+    const currMessage = currQuiz.message;
+
+    const characters = currQuiz.character;
+    console.log(characters);
+
+    const imageName = characters.toLowerCase().replace(/\s+/g, '-');
+    const imagePath = characterImages(`./${imageName}.png`);
+
     return (
         <div className="transition-container">
             <Navbar />
@@ -12,15 +28,15 @@ const Transition = () => {
                     <div className="message-side">
                         <div className="message-box">
                             <p className="transition-message">
-                                Use what you learned to Defeat Enemy 1 and Help me get back my information!!!
+                                {currMessage}
                             </p>
                             <button className='quiz-button'>TAKE THE QUIZ</button>
                         </div>
                     </div>
                     <div className="character-side">
                         <img 
-                            src={allieImage}
-                            alt="Allie"
+                            src={imagePath}
+                            alt= {characters}
                             className="character-image"
                         />
                     </div>
