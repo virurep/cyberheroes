d9c59df553a3c0d11a92b47651df445a27eeabab
vTempo <vincentkao2003@gmail.com>
2025-05-12 13:28:54 -0700

    new vocab, export processText

--- cyberheroes/src/components/Message.js
+++ cyberheroes/src/components/Message.js
@@ -3,80 +3,85 @@ import Buttons from './Buttons';
 import VocabPopup from './VocabPopup';
 import vocabData from '../data/lessons/vocab.json';
 
+
+export const processText = (text, onVocabClick) => {
+  return text.split('\n').map((paragraph, index) => {
+    // Split the paragraph by asterisks to find text to wrap in spans
+    // const parts = paragraph.split(/(\*[^*]+\*)/g);
+
+    const parts = paragraph.split(/(<[^*]+>[^*]+\*\*)/g);
+
+    return (
+      <p key={index}>
+        {parts.map((part, i) => {
+
+          // check if the part is a vocab word
+          if (part.startsWith("<v>") && part.endsWith("**")) {
+            const word = part.slice(3, -2);
+            const vocab = vocabData.words.find(w => w.word.toLowerCase() === word.toLowerCase());
+            if (vocab) {
+              return (
+                <span
+                  key={i}
+                  className="vocab-word"
+                  onClick={() => onVocabClick(vocab || { word })}
+                >
+                  {word}
+                </span>
+              );
+            }
+            return <span key={i} className="vocab-word">{word}</span>;
+          }
+
+          // check if the part is a list item
+          if (part.startsWith("<li>") && part.endsWith("**")) {
+            const item = part.slice(4, -2);
+            const listItems = item.split("<ul>");
+            console.log(item);
+            console.log(listItems);
+            return listItems.map((item, i) => {
+              return <li key={i}>{item}</li>;
+            });
+          }
+
+          // check if the part should be red
+          if (part.startsWith("<red>") && part.endsWith("**")) {
+            const text = part.slice(5, -2);
+            return <span className="red-text">{text}</span>;
+          }
+
+          // check if the part should be gold
+          if (part.startsWith("<gold>") && part.endsWith("**")) {
+            const text = part.slice(6, -2);
+            return <span className="gold-text">{text}</span>;
+          }
+
+          // check if the part should be underlined
+          if (part.startsWith("<u>") && part.endsWith("**")) {
+            const text = part.slice(3, -2);
+            return <span className="underline-text">{text}</span>;
+          }
+
+          return part;
+        })}
+      </p>
+    );
+  });
+};
+
 const Message = ({ message, onButtonClick }) => {
   const [selectedVocab, setSelectedVocab] = useState(null);
 
-  const processText = (text) => {
-    return text.split('\n').map((paragraph, index) => {
-      // Split the paragraph by asterisks to find text to wrap in spans
-      // const parts = paragraph.split(/(\*[^*]+\*)/g);
-
-      const parts = paragraph.split(/(<[^*]+>[^*]+\*\*)/g);
-
-      return (
-        <p key={index}>
-          {parts.map((part, i) => {
-
-            // check if the part is a vocab word
-            if (part.startsWith("<v>") && part.endsWith("**")) {
-              const word = part.slice(3, -2);
-              const vocab = vocabData.words.find(w => w.word.toLowerCase() === word.toLowerCase());
-              if (vocab) {
-                return (
-                  <span
-                    key={i}
-                    className="vocab-word"
-                    onClick={() => setSelectedVocab(vocab)}
-                  >
-                    {word}
-                  </span>
-                );
-              }
-              return <span key={i} className="vocab-word">{word}</span>;
-            }
-
-            // check if the part is a list item
-            if (part.startsWith("<li>") && part.endsWith("**")) {
-              const item = part.slice(4, -2);
-              const listItems = item.split("<ul>");
-              console.log(item);
-              console.log(listItems);
-              return listItems.map((item, i) => {
-                return <li key={i}>{item}</li>;
-              });
-            }
-
-            // check if the part should be red
-            if (part.startsWith("<red>") && part.endsWith("**")) {
-              const text = part.slice(5, -2);
-              return <span className="red-text">{text}</span>;
-            }
-
-            // check if the part should be gold
-            if (part.startsWith("<gold>") && part.endsWith("**")) {
-              const text = part.slice(6, -2);
-              return <span className="gold-text">{text}</span>;
-            }
-
-            // check if the part should be underlined
-            if (part.startsWith("<u>") && part.endsWith("**")) {
-              const text = part.slice(3, -2);
-              return <span className="underline-text">{text}</span>;
-            }
-
-            return part;
-          })}
-        </p>
-      );
-    });
+  const handleVocabClick = (vocab) => {
+    setSelectedVocab(vocab);
   };
 
-  const paragraphs = processText(message.text);
+  const paragraphs = processText(message.text, handleVocabClick);
 
   return (
     <div className="text-container">
       {message.header && (
-        <div className="text-header">{processText(message.header)}</div>
+        <div className="text-header">{processText(message.header, handleVocabClick)}</div>
       )}
       {message.speaker && (
         <div className={`speaker-name ${message.speaker_style}`}>
