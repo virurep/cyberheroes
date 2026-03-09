5749825855a26a56ad0143559fbbda5eed2b60ee
virurepalle <virurep@uw.edu>
2026-03-09 17:46:19 -0400

    fixed vuln

--- cyberheroes/src/index.js
+++ cyberheroes/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 
 import './styles/style.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+root.render(
+  <HashRouter>
+  <App/>
+</HashRouter>
 );
 
 
