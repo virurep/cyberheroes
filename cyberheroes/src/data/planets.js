5bd497e965c92df9cc3822eb715ac28f2a4dcb1a
virurepalle <virurep@uw.edu>
2026-04-30 12:19:21 -0400

    feat: add planets.js data registry (Task 5)

---
+++ cyberheroes/src/data/planets.js
@@ -0,0 +1,19 @@
+import privacyPlanetLesson from './privacy-planet/lesson.json';
+import privacyPlanetQuiz   from './privacy-planet/quiz.json';
+import privacyMoonLesson   from './privacy-moon/lesson.json';
+import privacyMoonQuiz     from './privacy-moon/quiz.json';
+import privacyMoonDragDrop from './privacy-moon/drag-drop.json';
+import privacyMoonRedFlag  from './privacy-moon/red-flag-green-flag.json';
+
+export const lessonData = {
+  'privacy-planet': privacyPlanetLesson,
+  'privacy-moon':   privacyMoonLesson,
+};
+
+export const quizData = {
+  'privacy-planet': privacyPlanetQuiz,
+  'privacy-moon':   privacyMoonQuiz,
+};
+
+export const dragDropData = privacyMoonDragDrop;
+export const redFlagData  = privacyMoonRedFlag;
