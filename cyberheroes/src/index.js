fe6cd5754b2fa082ada437d67a5cd4478034b337
virurepalle <virurep@uw.edu>
2026-05-10 13:30:43 -0700

    feat: wire Press Start 2P font and design tokens globally

--- cyberheroes/src/index.js
+++ cyberheroes/src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from "react-router-dom";
 
+import './styles/tokens.css';
 import './styles/style.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-  <App/>
-</HashRouter>
+    <App />
+  </HashRouter>
 );
-
-
