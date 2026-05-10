7903df1ced46bf575a1537e1306557a0a230f603
virurepalle <virurep@uw.edu>
2026-05-10 13:34:24 -0700

    feat: restyle navbar with dark pixel aesthetic

--- cyberheroes/src/components/util/NavBar.js
+++ cyberheroes/src/components/util/NavBar.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import logo from '../../img/general/logo.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../../styles/navbar.css';
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <Link to="/" className="logo-link">
           <img src={logo} alt="Cyberheroes Logo" className="navbar-logo" />
         </Link>
-        <Link to="/" className="title-link">
-          <div className="navbar-title">Cyberheroes</div>
+        <Link to="/" className="logo-link">
+          <span className="navbar-title">
+            <span className="cyber">CYBER</span>
+            <span className="heroes">HEROES</span>
+          </span>
         </Link>
       </div>
       <div className="navbar-right">
-        <Link to="/about" className="home-button">About</Link>
-        <Link to="/exploration-map" className="home-button">Home</Link>
+        <Link to="/about" className={`nav-link${pathname === '/about' ? ' active' : ''}`}>ABOUT</Link>
+        <Link to="/exploration-map" className={`nav-link${pathname === '/exploration-map' ? ' active' : ''}`}>HOME</Link>
       </div>
     </nav>
   );
