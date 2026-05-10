54187d639890084001e4101ea1fc1bc29791d701
virurepalle <virurep@uw.edu>
2026-05-10 14:09:56 -0700

    feat: restyle lesson pages and wire Knight component into character renderer

--- cyberheroes/src/components/lessons/Characters.js
+++ cyberheroes/src/components/lessons/Characters.js
@@ -1,46 +1,78 @@
-import arrow from '../../img/general/arrow.png'
+import arrow from '../../img/general/arrow.png';
+import Knight from '../util/Knight';
+
 const characterImages = require.context('../../img/characters', false, /\.(png|jpe?g|svg)$/);
 
+// Map character names to Knight color presets
+const KNIGHT_COLOR_MAP = {
+  'allie': 'cyan',
+  'al': 'cyan',
+  'alejandro': 'purple',
+  'enemy': 'red',
+  'patrick': 'red',
+  'privacy-enemy': 'red',
+  'safe': 'green',
+  'cyber-hero': 'gold',
+};
 
 const Characters = ({ characters }) => {
   return (
     <div className="character-container">
       {characters.map((character) => {
-        // Convert character name to match your image filename
         const imageName = character.name.toLowerCase().replace(/\s+/g, '-');
-        const imagePath = characterImages(`./${imageName}.png`);
-
+        const knightColor = KNIGHT_COLOR_MAP[imageName];
 
         if (character.arrow) {
-          // clickable character to go to the next page
-          const handleClick = (page) => {
-            character.onClick(page);
-          }
+          const handleClick = () => character.onClick(character.arrow);
           const handleKeyDown = (e) => {
-            if (e.key === 'Enter' || e.key === ' ') {
-              handleClick(character.arrow);
-            }
+            if (e.key === 'Enter' || e.key === ' ') handleClick();
           };
           return (
             <div key={character.name} className={`arrow-character ${character.style}`}>
-              <div className='arrow-container'>
-                  <img src={arrow} alt='arrow' className='arrow' />
+              <div className="arrow-container">
+                <img src={arrow} alt="arrow" className="arrow" />
               </div>
-              <img
-                src={imagePath}
-                alt={character.name}
-                className={`character character-arrow`}
-                onClick={() => handleClick(character.arrow)}
-                role="button"
-                tabIndex={0}
-                onKeyDown={handleKeyDown}
-              />
+              {knightColor ? (
+                <div
+                  className={`character character-arrow`}
+                  onClick={handleClick}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={handleKeyDown}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <Knight size={200} color={knightColor} animate={false} />
+                </div>
+              ) : (
+                <img
+                  src={characterImages(`./${imageName}.png`)}
+                  alt={character.name}
+                  className="character character-arrow"
+                  onClick={handleClick}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={handleKeyDown}
+                />
+              )}
+            </div>
+          );
+        }
+
+        if (knightColor) {
+          return (
+            <div key={character.name} className={`character ${character.style}`}>
+              <Knight size={240} color={knightColor} animate={true} />
             </div>
           );
         }
 
         return (
-          <img key={character.name} src={imagePath} alt={character.name} className={`character ${character.style}`} />
+          <img
+            key={character.name}
+            src={characterImages(`./${imageName}.png`)}
+            alt={character.name}
+            className={`character ${character.style}`}
+          />
         );
       })}
     </div>
